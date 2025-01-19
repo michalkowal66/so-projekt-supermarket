ce0a07aaad110753f656041b194969ca0c98de6c
Michal Kowal <michal.kowal.66@gmail.com>
2025-01-19 17:14:35 +0100

    Include client_factory in build script

--- build.sh
+++ build.sh
@@ -2,3 +2,5 @@ g++ ./src/manager.cpp ./src/shared.cpp -o ./bin/manager
 g++ ./src/cashier.cpp ./src/shared.cpp -o ./bin/cashier
 g++ ./src/client.cpp ./src/shared.cpp -o ./bin/client
 g++ ./src/fireman.cpp ./src/shared.cpp -o ./bin/fireman
+g++ ./src/client_factory.cpp -o ./bin/client_factory
+
