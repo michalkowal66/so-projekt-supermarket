6a5a20504b8c6eb874c0f0be283ce179ac1b855a
Michał Kowal <michal.kowal.66@gmail.com>
2025-01-26 19:04:17 +0100

    Include missing lpthread flag in build script

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
-g++ ./src/manager.cpp ./src/shared.cpp ./src/ccol.cpp -o ./bin/manager
-g++ ./src/cashier.cpp ./src/shared.cpp ./src/ccol.cpp -o ./bin/cashier
-g++ ./src/client.cpp ./src/shared.cpp ./src/ccol.cpp -o ./bin/client
-g++ ./src/fireman.cpp ./src/shared.cpp ./src/ccol.cpp -o ./bin/fireman
-g++ ./src/client_factory.cpp ./src/ccol.cpp -o ./bin/client_factory
+g++ ./src/manager.cpp ./src/shared.cpp ./src/ccol.cpp -lpthread -o ./bin/manager
+g++ ./src/cashier.cpp ./src/shared.cpp ./src/ccol.cpp -lpthread -o ./bin/cashier
+g++ ./src/client.cpp ./src/shared.cpp ./src/ccol.cpp -lpthread -o ./bin/client
+g++ ./src/fireman.cpp ./src/shared.cpp ./src/ccol.cpp -lpthread -o ./bin/fireman
+g++ ./src/client_factory.cpp ./src/ccol.cpp -lpthread -o ./bin/client_factory
 
