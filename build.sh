5268e56e46fe945763b750140983ae815ceb45f5
Michał Kowal <michal.kowal.66@gmail.com>
2025-01-26 21:57:08 +0100

    Makes sure bin dir exists before compiling

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
+mkdir -p ./bin
 g++ ./src/manager.cpp ./src/shared.cpp ./src/ccol.cpp -lpthread -o ./bin/manager
 g++ ./src/cashier.cpp ./src/shared.cpp ./src/ccol.cpp -lpthread -o ./bin/cashier
 g++ ./src/client.cpp ./src/shared.cpp ./src/ccol.cpp -lpthread -o ./bin/client
 g++ ./src/fireman.cpp ./src/shared.cpp ./src/ccol.cpp -lpthread -o ./bin/fireman
 g++ ./src/client_factory.cpp ./src/ccol.cpp -lpthread -o ./bin/client_factory
-
